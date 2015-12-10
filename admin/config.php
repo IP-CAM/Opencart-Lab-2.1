60a670d4a8a85487e005c0183622384f69611633
Sergio Aguilera <sergio.aaguilera@gmail.com>
2015-12-09 18:17:43 -0600

    Creado tema Testing
    creado tema  para pruebas

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/open/admin/');
+define('HTTP_CATALOG', 'http://localhost/open/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/open/admin/');
+define('HTTPS_CATALOG', 'http://localhost/open/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/open/admin/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/open/system/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/open/admin/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/open/admin/view/template/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/open/system/config/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/open/image/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/open/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/open/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/open/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/open/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/open/system/storage/upload/');
+define('DIR_CATALOG', '/Applications/XAMPP/xamppfiles/htdocs/open/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'prueba_open');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
